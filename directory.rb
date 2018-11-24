772cf29c93036b445e145e17b45b44e5d671cab6
EC2 Default User <ec2-user@ip-172-31-27-227.us-east-2.compute.internal>
2018-11-24 18:34:04 +0000

    Step8ex1 Completed

--- directory.rb
+++ directory.rb
@@ -1,6 +1,5 @@
 
 #first we print the list of students
-
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
@@ -22,12 +21,13 @@ def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+  students.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
-
 def print_footer(names)
   puts "Overall we have #{names.count} great students."
 end
 print_header
 print(students)
 print_footer(students)
-
