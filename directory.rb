f920f3038237910e292bb88181fc9ee3424c5ce8
EC2 Default User <ec2-user@ip-172-31-27-227.us-east-2.compute.internal>
2018-11-26 05:24:38 +0000

    Removed need to end program manually

--- directory.rb
+++ directory.rb
@@ -87,20 +87,18 @@ end
 def save_students
   puts "What do you want to save the file as?" 
   saved_file = gets.chomp 
-  file = File.open(saved_file, "w")  
+  file = File.open(saved_file, "w") do |file| 
   @students.each do |student| 
     student_data = [student[:name], student[:cohort]] 
     csv_line = student_data.join(",") 
     file.puts csv_line 
-   file.close 
 end 
 def load_students(filename = "students.csv") 
-  file = File.open(filename, "r") 
+  file = File.open(filename, "r") do |file| 
   file.readlines.each do |line| 
   name, cohort = line.chomp.split(',') 
     add_students(name, cohort) 
   end 
-  file.close 
 end
 def try_load_students 
   puts "Enter a filename to load" 
